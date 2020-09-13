7d40e73507c4bce8700309e1438e5878c9487e51
Shriya Challam <shriyachallam10@gmail.com>
2020-09-12 21:41:04 -0700

    'tests'

--- tests/test_schedules.py
+++ tests/test_schedules.py
@@ -1,8 +1,15 @@
-from unittest import TestCase
+import unittest
 import maccorcyclingdata.schedules as schedules
 import os
 
-class TestJoke(TestCase):
-    def test_is_string(self):
-        s = funniest.joke()
-        self.assertTrue(isinstance(s, basestring))
+expath = "example_data/"
+exfile = "schedule.csv"
+
+def test_schedules_columns():
+    df = schedules.import_schedules(expath , exfile)
+    columns = list(df.columns)
+    correct_columns = ['step', 'step_type', 'step_mode', 'step_mode_value', 'step_limit', 'step_limit_value', 'step_end_type', 'step_end_type_op', 'step_end_type_value', 'goto_step', 'report_type', 'report_type_value', 'options', 'step_note']
+    assert(columns == correct_columns)
+
+#def test_scheduler_steps_order(self):
+#    df = schedules.import_schedules(expath , exfile)
--- tests/test_testdata.py
+++ tests/test_testdata.py
@@ -1,12 +1,17 @@
-from unittest import TestCase
+import unittest
 import maccorcyclingdata.testdata as testdata
 import os
 
 expath = "example_data/"
-exmult = "example_data/multiple_csv"
+exmult = "example_data/multiple_csv/"
 exfile = "testdata.csv"
 
-class test_column():
-    def test_is_string(self):
-        s = funniest.joke()
-        self.assertTrue(isinstance(s, basestring))
+
+def test_data_is_csv():
+    assert((expath + exfile).endswith(".csv"))
+
+def test_columns_check():
+    df = testdata.import_maccor_data(expath , exfile, header=0)
+    columns = list(df.columns)
+    correct_columns = ['cyc', 'step', 'test_time_s', 'step_time_s', 'capacity_mah', 'current_ma', 'voltage_v', 'dpt_time', 'thermocouple_temp_c', 'ev_temp']
+    assert(columns == correct_columns)
